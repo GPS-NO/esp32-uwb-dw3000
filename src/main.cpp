405a989330012cb79d941d1d7758a4c0dc302fde
Gurkengewuerz <niklas@schuetrumpf.net>
2024-02-16 13:00:06 +0100

    feat: add startup notices

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,9 @@
 #include <Arduino.h>
 #include <Wire.h>
-#include "state_machine_state.h"
+
 #include "idle_state.h"
 #include "setup_state.h"
+#include "state_machine_state.h"
 
 const int flashBtnPin = 0;
 
@@ -10,14 +11,24 @@ State* StateMachineState::idleState = new IdleState();
 State* StateMachineState::setupState = new SetupState();
 State* StateMachineState::currentState = StateMachineState::idleState;
 
-void setup(){
+void setup() {
     Serial.begin(115200);
-    while (!Serial) {;}
+    while (!Serial) {
+        ;
+    }
 
     pinMode(flashBtnPin, INPUT_PULLUP);
+
+    Serial.println(F("###################################################"));
+    Serial.println(F("(c) 2023-2024 Hochschule Bochum GPS:NO - Martin Peth, Niklas Schuetrumpf"));
+    Serial.print(F("Compiled with c++ version "));
+    Serial.println(__VERSION__);
+    Serial.printf("Version v%s @%s %s at %s", VERSION_STRING, GIT_COMMIT, __DATE__, __TIME__);
+    Serial.println();
+    Serial.println(F("###################################################"));
 }
 
-void loop(){
-    StateMachineState::currentState->onEnter(); 
-    StateMachineState::currentState->onUpdate();    
+void loop() {
+    StateMachineState::currentState->onEnter();
+    StateMachineState::currentState->onUpdate();
 }
